ab57ee9df175487c65b911401f9ea8115e6708b1
soloman <markalt4321@gmail.com>
2026-08-11 15:58:21 -0700

    Add project files.

---
+++ SubModule.cs
@@ -0,0 +1,26 @@
+﻿using TaleWorlds.MountAndBlade;
+
+
+namespace Convoys
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+
+        }
+
+        protected override void OnSubModuleUnloaded()
+        {
+            base.OnSubModuleUnloaded();
+
+        }
+
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+
+        }
+    }
+}
